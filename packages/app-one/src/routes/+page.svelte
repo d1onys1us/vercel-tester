951e56ce756ce139bd07add295ef79f04d026b6d
d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2024-02-18 22:40:26 -0500

    feat(app-one): hotfix again

--- packages/app-one/src/routes/+page.svelte
+++ packages/app-one/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit this shouldnt show in prod!</h1>
+<h1>Welcome to SvelteKit this shullddd show in prod as a hotfix</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to reaiiiid the documentation. Test the action. lets get this to preview! and then prod! ok will it release</p>
