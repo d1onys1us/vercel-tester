a82d609f06bee7a4e260ddc4710cc3a4a958c592
d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2024-02-18 22:38:49 -0500

    feat(app-one): add to prod (#20)

--- packages/app-one/src/routes/+page.svelte
+++ packages/app-one/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit this shouldnt show in prod!</h1>
+<h1>Welcome to SvelteKit this shouldnt show in prod! i repeat should not</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to reaiiiid the documentation. Test the action. lets get this to preview! and then prod! ok will it release</p>
